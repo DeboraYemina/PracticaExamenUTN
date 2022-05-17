adb648e435afd706f18c2f72580c28a6778ef124
deborayemina <deborayemina@gmail.com>
2022-05-17 00:45:50 -0300

    a medio configurara clase persona

--- EntidadesAbstractas/Persona.cs
+++ EntidadesAbstractas/Persona.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace EntidadesAbstractas
 {
-    internal class Persona
+    internal abstract class Persona
     {
+        private string _apellido;
+        private int _dni;
+        private ENacionalidad _nacionalidad;
+        private string _nombre;
+
+        public string Apellido { get; set; }
+        public int DNI { get; set; }
+        public ENacionalidad Nacionalidad { get; set; }
+        public string Nombre { get; set; }
+        public string StringToDNI { get; set; }
+
+        public Persona (string nombre, string apellido, ENacionalidad nacionalidad)
+        { }
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+        { }
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+        { }
+
+        public override string ToString()
+        { }
+
     }
 }
