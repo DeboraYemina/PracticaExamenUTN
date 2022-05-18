fcc2c2724240f117fe9f89d6d06ba7c6bc13cb96
deborayemina <deborayemina@gmail.com>
2022-05-18 01:52:17 -0300

    comenzando

--- EntidadesAbstractas/Persona.cs
+++ EntidadesAbstractas/Persona.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EntidadesAbstractas
 {
-    internal abstract class Persona
+    abstract class Persona
     {
         private string _apellido;
         private int _dni;
@@ -14,7 +14,17 @@ namespace EntidadesAbstractas
         private string _nombre;
 
         public string Apellido { get; set; }
-        public int DNI { get; set; }
+        public int DNI 
+        {
+            get 
+            { 
+                return _dni; 
+            }
+            set 
+            { 
+                _dni = ValidarDNni(_nacionalidad, _dni); 
+            } 
+        }
         public ENacionalidad Nacionalidad { get; set; }
         public string Nombre { get; set; }
         public string StringToDNI { get; set; }
@@ -28,8 +38,11 @@ namespace EntidadesAbstractas
 
         public override string ToString()
         { }
-        private int ValidarDNni (ENacionalidad nacionalidad, int dato)
-        { }
+        private int ValidarDni (ENacionalidad nacionalidad, int dato)
+        {
+            //validar dni correcto, argentino entre 1 y 89999999, sino lanzar excepcion dniinvalidoexception
+            return 5;
+        }
         private int ValidarDNni(ENacionalidad nacionalidad, string dato)
         { }
         private string ValidarNombreApellido (string dato)
--- EntidadesInstanciables/Gimnasio.cs
+++ EntidadesInstanciables/Gimnasio.cs
@@ -8,5 +8,41 @@ namespace EntidadesInstanciables
 {
     internal class Gimnasio
     {
+        private List<Alumno> _alumnos;
+        private List<Instructor> _instructores;
+        private List<Jornada> _jornada;
+
+        public Gimnasio()
+        { }
+        public bool Guardar(Gimnasio gim)
+        { }
+        private string MostrarDatos(Gimnasio gim)
+        { }
+        public string ToString()
+        { }
+        public bool operator !=(Gimnasio g, Alumno a)
+        { }
+        public Instructor operator !=(Gimnasio g, EClase clase)
+        { }
+        public bool operator !=(Gimnasio g, Instructor i)
+        { }
+        public Gimnasio operator +(Gimnasio g, Alumno a)
+        { }
+        public Gimnasio operator +(Gimnasio g, EClase clase)
+        { }
+        public Gimnasio operator +(Gimnasio g, Instructor i)
+        { }
+        public bool operator ==(Gimnasio g, Alumno a)
+        { }
+        public Instructor operator ==(Gimnasio g, EClase clase)
+        { }
+        public bool operator ==(Gimnasio g, Instructor i)
+        { }
+        public Gimnasio operator -(Gimnasio g, Alumno a)
+        { }
+        public Gimnasio operator -(Gimnasio g, EClase clase)
+        { }
+        public Gimnasio operator -(Gimnasio g, Instructor i)
+        { }
     }
 }
